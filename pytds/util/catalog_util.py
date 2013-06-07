5a7de66c4332b4a7282e6fbc95e0fe355ff763af
Julien Chastang <julien.c.chastang@gmail.com>
2013-06-07 13:57:21 -0600

    fixing tabs

--- pytds/util/catalog_util.py
+++ pytds/util/catalog_util.py
@@ -19,6 +19,7 @@ def get_url_path(url):
     parts = o.path.split('/')
     return o.scheme + "://" + o.netloc + '/'.join(parts[:-1])
 
+
 def get_element_root_from_url(url):
     """Given a XML URL, return an element root"""
     data = urllib2.urlopen(url)
@@ -26,18 +27,20 @@ def get_element_root_from_url(url):
     data.close()
     return root
 
+
 def find_dataset(root, service_name):
     """
     Given an  element root, and  a service name, recursively  find the
     first dataset with that service name
     """
     for ds in root.findall(xmlns_prefix + 'dataset'):
-	sn = ds.find(xmlns_prefix + "serviceName")
-	if ((sn is not None) and (service_name == sn.text)):
-	    return ds
-	else:
-	    if find_dataset(ds, service_name) is not None:
-		return find_dataset(ds, service_name)
+        sn = ds.find(xmlns_prefix + "serviceName")
+    if ((sn is not None) and (service_name == sn.text)):
+        return ds
+    else:
+        if find_dataset(ds, service_name) is not None:
+            return find_dataset(ds, service_name)
+
 
 def get_resolver_xml_url(dataset_url):
     """Given a dataset URL, return the resolver XML URL."""
@@ -46,14 +49,15 @@ def get_resolver_xml_url(dataset_url):
     latest_base = ''
     latest_url = None
     for s in root.findall(xmlns_prefix + 'service'):
-    	if (s.get("serviceType") == "Resolver"):
-	   latest = s.get("name")
-    	   latest_base = s.get("base")
+        if (s.get("serviceType") == "Resolver"):
+            latest = s.get("name")
+            latest_base = s.get("base")
     ds = find_dataset(root,latest)
     if (ds is not None):
-	# TODO: generalize this to handle relatives paths starting with a '/'	   
-	latest_url = get_url_path(dataset_url) + latest_base + '/' + ds.get('urlPath')
+        # TODO: generalize this to handle relatives paths starting with a '/'       
+        latest_url = get_url_path(dataset_url) + latest_base + '/' + ds.get('urlPath')
     return latest_url
+
 
 def get_service_endpoint(root, service):
     """
@@ -62,9 +66,9 @@ def get_service_endpoint(root, service):
     #TODO: Deal with suffix
     service_dict = {}
     for s in root.findall(xmlns_prefix + 'service'):
-    	if (s.get("serviceType") == "Compound"):
-	   for child in s:
-	       service_dict[child.get("serviceType")] = child.get("base")
+        if (s.get("serviceType") == "Compound"):
+            for child in s:
+                service_dict[child.get("serviceType")] = child.get("base")
     return service_dict[service]
 
 
@@ -74,7 +78,7 @@ def get_latest_dods_url(dataset_url):
     resolver = get_resolver_xml_url(dataset_url)
     latest = None
     if (resolver is not None):
-	root = get_element_root_from_url(resolver)
-	ds = root.find(xmlns_prefix + 'dataset')
-	latest = o.scheme + "://" + o.netloc + get_service_endpoint(root,'OPENDAP') + ds.get('urlPath')
+        root = get_element_root_from_url(resolver)
+        ds = root.find(xmlns_prefix + 'dataset')
+        latest = o.scheme + "://" + o.netloc + get_service_endpoint(root,'OPENDAP') + ds.get('urlPath')
     return latest
