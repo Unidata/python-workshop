86abfa1967851e7a7d49dcd59533caee300ade6e
Sean Arms <sarms@unidata.ucar.edu>
2013-06-11 11:21:28 -0600

    updated radar level II example...preparing to convert to ipython notebook

--- previous_tds_python_scripts/radar_level_II/example.py
+++ previous_tds_python_scripts/radar_level_II/example.py
@@ -1,7 +1,39 @@
 import string
 import numpy as np
+import matplotlib as mpl
 from pydap.client import open_url
 from xml.dom import minidom as md
+import matplotlib.pyplot as plt
+def radar_colormap():
+    nws_reflectivity_colors = [
+    "#646464", # ND
+    "#ccffff", # -30
+    "#cc99cc", # -25
+    "#996699", # -20
+    "#663366", # -15
+    "#cccc99", # -10
+    "#999966", # -5
+    "#646464", # 0
+    "#04e9e7", # 5
+    "#019ff4", # 10
+    "#0300f4", # 15
+    "#02fd02", # 20
+    "#01c501", # 25
+    "#008e00", # 30
+    "#fdf802", # 35
+    "#e5bc00", # 40
+    "#fd9500", # 45
+    "#fd0000", # 50
+    "#d40000", # 55
+    "#bc0000", # 60
+    "#f800fd", # 65
+    "#9854c6", # 70
+    "#fdfdfd" # 75
+    ]
+
+    cmap = mpl.colors.ListedColormap(nws_reflectivity_colors)
+
+    return cmap
 
 def basic_http_request(full_url, return_response = False):
     #
@@ -58,9 +90,9 @@ def get_latest_data_url(site,date):
     return latest_data_url
 
 # basic input
-site = "KTLX"
-date = "20130429"
-variable = "Reflectivity_HI"
+site = "KBRO"
+date = "20130611"
+variable = "Reflectivity"
 scan = 0
 #
 # get latest data url
@@ -77,18 +109,37 @@ lon = global_attrs["StationLongitude"]
 station_id = global_attrs["StationName"]
 
 # get data array and metadata
-data = dataset["Reflectivity_HI"][scan,::].squeeze()
-theta = dataset["azimuthR_HI"][scan,::].squeeze()
-r = dataset["distanceR_HI"][:]
-data_attrs = dataset["Reflectivity_HI"].attributes
+data = dataset["Reflectivity"][scan,::].squeeze()
+theta = dataset["azimuthR"][scan,::].squeeze()
+r = dataset["distanceR"][:]
+data_attrs = dataset["Reflectivity"].attributes
 
 # check for scale and offset
-if data_attrs.has_key("scale"):
-    data = data * data_attrs["scale"]
+if data_attrs.has_key("scale_factor"):
+    data = data * data_attrs["scale_factor"]
 
 if data_attrs.has_key("add_offset"):
     data = data + data_attrs["add_offset"]
 
+data = np.ma.masked_array(data, data < 5)
+
 print("number of radials: {}".format(r.shape[0],))
 print("number of azimuth angles: {}".format(theta.shape[0],))
 print("dimensions of data: ({}, {})".format(data.shape[0],data.shape[1]))
+
+theta = theta * np.pi / 180.
+r = r / 1000.
+x = np.array((np.matrix(np.cos(theta)).transpose() * np.matrix(r)))
+y = np.array((np.matrix(np.sin(theta)).transpose() * np.matrix(r)))
+
+cmap = radar_colormap()
+norm = mpl.colors.Normalize(vmin=-35, vmax=80)
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.plot(0, 0, '+')
+cax = ax.pcolormesh(y,x,data, cmap=cmap, norm=norm)
+ax.set_xlim(-200,200)
+ax.set_ylim(-200,200)
+cbar = fig.colorbar(cax)
+plt.show()
