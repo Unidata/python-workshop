62f1a39e2e53888215e8bbe2cb47097a4895757b
Ward Fisher <wfisher@unidata.ucar.edu>
2014-08-26 14:27:18 -0600

    Updated bootstrap for 32, 64 bit.  Can now use ipython notebook with a browser on the host machine.  Added an MOTD relating to the workshop.

--- Vagrantfile
+++ Vagrantfile
@@ -20,12 +20,14 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.define "workshop64", primary: true do |v|
     v.vm.provision :shell, :path => "bootstrap_vm.sh", :args => "64"
     v.vm.hostname = "workshop64"
+    v.vm.network "forwarded_port", guest: 8888, host: 8864, protocol: 'tcp'
     v.vm.box = "ubuntu/trusty64"
   end
 
   config.vm.define "workshop32" do |v|
     v.vm.provision :shell, :path => "bootstrap_vm.sh", :args => "32"
     v.vm.hostname = "workshop32"
+    v.vm.network "forwarded_port", guest: 8888, host: 8832, protocol: 'tcp'
     v.vm.box = "ubuntu/trusty32"
   end
 
--- bootstrap_vm.sh
+++ bootstrap_vm.sh
@@ -19,7 +19,7 @@ fi
 #apt-get -y upgrade
 
 # Install some packages
-sudo apt-get install -y git emacs24 firefox
+sudo apt-get install -y git emacs24 firefox nethogs htop wget
 
 # Add the anaconda path to the vagrant users path.
 sudo -u vagrant echo 'PATH=/home/vagrant/anaconda/bin:$PATH' >> /home/vagrant/.bashrc
@@ -30,25 +30,67 @@ sudo -u vagrant echo 'PATH=/home/vagrant/anaconda/bin:$PATH' >> /home/vagrant/.b
 
 
 if [ ! -f "/vagrant/$INSTFILE" ]; then
-    echo "Error: Need to download anaconda install file by hand and place in host directory."
-    echo "File: $INSTFILE"
-    exit 1
+    wget http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/$INSTFILE
+
+    if [ ! -f "/home/vagrant/$INSTFILE" ]; then
+        echo "Error downloading $INSTFILE."
+        exit 1
+    fi
+
+    cp $INSTFILE /vagrant/
+else
+    sudo -u vagrant cp /vagrant/$INSTFILE /home/vagrant
 fi
 
-cp /vagrant/$INSTFILE /home/vagrant
 # Install Anaconda
-sudo -u vagrant bash $INSTFILE -bf -p /home/vagrant/anaconda
+sudo -u vagrant bash /home/vagrant/$INSTFILE -bf -p /home/vagrant/anaconda
 
 # Install some conda packages
 sudo -u vagrant /home/vagrant/anaconda/bin/conda create --yes -n workshop python numpy matplotlib netcdf4 basemap pandas ipython pyzmq jinja2 tornado sympy
 
-sudo -u vagrant /home/vagrant/anaconda/bin/conda install --yes --quiet -n workshop -c https://conda.binstar.org/rsignell owslib
+# Rich only makes owslib availabe for 64-bit. I'll try to build
+# and make available for 32-bit.
+if [ "$1" == "64" ]; then
+    sudo -u vagrant /home/vagrant/anaconda/bin/conda install --yes --quiet -n workshop -c https://conda.binstar.org/rsignell owslib
+else
+    sudo -u vagrant /home/vagrant/anaconda/bin/conda install --yes --quiet -n workshop -c https://conda.binstar.org/wardf owslib
+fi
 
 sudo -u vagrant /home/vagrant/anaconda/bin/conda install --yes --quiet -n workshop -c https://conda.binstar.org/Unidata pyudl
 
+#
 # Create a text file explaining how to activate the workshop.
+#
+
 OFILE="/home/vagrant/WORKSHOP_README.txt"
-echo -e "TODO: COMPLETE THIS FILE\n\n" > $OFILE
+echo -e "Unidata Python Workshop\n\n" > $OFILE
+echo -e "This Virtual Machine provides a complete environment" >> $OFILE
+echo -e "for the Unidata Python workshop.\n" >> $OFILE
+echo -e "Usage:\n" >> $OFILE
+echo -e '$ cd /vagrant' >> $OFILE
+echo -e '$ ipython notebook' >> $OFILE
+echo -e ""
+echo -e "This will launch ipython notebook in firefox over x11." >> $OFILE
+echo -e "Alternatively, you may open a browser on your" >> $OFILE
+echo -e "host machine and navigate to:" >> $OFILE
+if [ "$1" == "64" ]; then
+    echo -e "\thttp://127.0.0.1:8864" >> $OFILE
+else
+    echo -e "\thttp://127.0.0.1:8832" >> $OFILE
+fi
+echo -e "\nIf you want to use an external browser from" >> $OFILE
+echo -e "your host machine, you must start ipython notebook" >> $OFILE
+echo -e "as follows:" >> $OFILE
+echo -e "$ ipython notebook --ip=*" >> $OFILE
+
+echo -e "\nNote: the /vagrant/ directory is mapped to" >> $OFILE
+echo -e "the unidata-python-workshop/ directory on your" >> $OFILE
+echo -e "host machine.  This way any changes made to the" >> $OFILE
+echo -e "notebooks will not dissapear if the VM is destroyed!" >> $OFILE
+
+#
+# End workshop readme.
+#
 
 # Have the user see the WORKSHOP_README.txt file every time the log in.
 sudo -u vagrant echo 'if [ -f "/home/vagrant/WORKSHOP_README.txt" ]; then' >> /home/vagrant/.bashrc
