372e8977149438b22be9d23f9799404558f620b6
John Leeman <kd5wxb@gmail.com>
2017-12-18 15:00:28 -0700

    Flake and cleanup solutions.

--- notebooks/MetPy_Case_Study/solutions/temp_adv.py
+++ notebooks/MetPy_Case_Study/solutions/temp_adv.py
@@ -1,2 +1,3 @@
 # Temperature Advection
-tmpc_adv_850 = mpcalc.advection(tmpk_850, [uwnd_850,vwnd_850], (dx, dy), dim_order='yx').to('degC/s')
+tmpc_adv_850 = mpcalc.advection(tmpk_850, [uwnd_850, vwnd_850],
+                                (dx, dy), dim_order='yx').to('degC/s')
