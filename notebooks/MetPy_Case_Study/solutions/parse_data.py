8b5c29e3e0d26bd1df9276531fa2612a394707af
J.R. Leeman <kd5wxb@gmail.com>
2017-12-19 10:57:53 -0700

    Format exercises to be consistent with the repo style.

--- notebooks/MetPy_Case_Study/solutions/parse_data.py
+++ notebooks/MetPy_Case_Study/solutions/parse_data.py
@@ -1,16 +1,11 @@
-# Grab data and assign units
+# Extract data and assign units
 tmpk = gaussian_filter(data.variables['Temperature'][0], sigma=1.0) * units.K
 hght = gaussian_filter(data.variables['Geopotential_height'][0],
                        sigma=1.0) * units.meter
 uwnd = gaussian_filter(data.variables['u_wind'][0], sigma=1.0) * units('m/s')
 vwnd = gaussian_filter(data.variables['v_wind'][0], sigma=1.0) * units('m/s')
 
-# Grab data for plotting
+# Extract coordinate data for plotting
 lat = data.variables['lat'][:]
 lon = data.variables['lon'][:]
 lev = data.variables['isobaric'][:]
-time = data.variables['time']
-vtime = num2date(time[0], units=time.units)
-
-# Calcualte dx and dy for calculations
-dx, dy = calc_dx_dy(lon, lat)
