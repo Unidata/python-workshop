56f8715d40cd52e70ab671ebc2cb100421aea4fb
kgoebber <kevin.goebbert@valpo.edu>
2018-12-04 17:10:57 -0600

    add QG Analysis workshop

---
+++ notebooks/MetPy_Advanced/solutions/qg_omega_total_fig.py
@@ -0,0 +1,36 @@
+fig=plt.figure(1, figsize=(15.,12.))
+
+# Upper-Left Panel
+ax=plt.subplot(111,projection=plotproj)
+ax.set_extent([-125.,-73,25.,50.],ccrs.PlateCarree())
+ax.coastlines('50m', linewidth=0.75)
+ax.add_feature(cfeature.STATES,linewidth=0.5)
+
+# Contour #1
+cs = ax.contour(lons, lats, hght_700s, clev_hght_700,colors='k',
+                linewidths=1.5, linestyles='solid', transform=dataproj)
+plt.clabel(cs, fontsize=10, inline=1, inline_spacing=3, fmt='%i',
+           rightside_up=True, use_clabeltext=True)
+
+# Contour #2
+cs2 = ax.contour(lons, lats, tmpc_700s, clev_tmpc_700, colors='grey',
+                linewidths=1.0, linestyles='dotted', transform=dataproj)
+plt.clabel(cs2, fontsize=10, inline=1, inline_spacing=3, fmt='%d',
+           rightside_up=True, use_clabeltext=True)
+
+# Colorfill
+cf = ax.contourf(lons, lats, (term_A+term_B)*10**12, clev_omega,
+                 cmap=plt.cm.RdYlBu_r, extend='both', transform=dataproj)
+plt.colorbar(cf, orientation='horizontal', pad=0.0, aspect=50, extendrect=True)
+
+# Vector
+ax.barbs(lons, lats, uwnd_700s.to('kts').m, vwnd_700s.to('kts').m,
+         regrid_shape=15, transform=dataproj)
+
+# Titles
+plt.title(r'700-hPa Geopotential Heights, Temperature (C), QG Omega Forcings '
+          '($*10^{12}$ kg m$^{-3}$ s$^{-3}$), and Winds (kt)',loc='left')
+plt.title('VALID: %s' %(vtime),loc='right')
+
+plt.tight_layout()
+plt.show()
