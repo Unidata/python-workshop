dde5963d566bb04ee83b0300ca65aa65111398cf
Jon Thielen <github@jont.cc>
2018-12-18 14:16:19 -0600

    Update QG Analysis to use MetPy 0.10 features

--- notebooks/MetPy_Advanced/solutions/QG_data.py
+++ notebooks/MetPy_Advanced/solutions/QG_data.py
@@ -1,16 +1,11 @@
 # Remaining variables needed to compute QG Omega forcing terms
-hght_500 = (ds.Geopotential_height_isobaric.sel({lev_name:500,
-                                                time_name:'{:%Y-%m-%d}'.format(dt)}).values
-            * units(ds.Geopotential_height_isobaric.units))
-uwnd_500 = (ds['u-component_of_wind_isobaric'].sel({lev_name:500,
-                                                   time_name:'{:%Y-%m-%d}'.format(dt)}).values
-            * units(ds['u-component_of_wind_isobaric'].units))
-vwnd_500 = (ds['v-component_of_wind_isobaric'].sel({lev_name:500,
-                                                   time_name:'{:%Y-%m-%d}'.format(dt)}).values
-            * units(ds['v-component_of_wind_isobaric'].units))
-uwnd_900 = (ds['u-component_of_wind_isobaric'].sel({lev_name:900,
-                                                   time_name:'{:%Y-%m-%d}'.format(dt)}).values
-            * units(ds['u-component_of_wind_isobaric'].units))
-vwnd_900 = (ds['v-component_of_wind_isobaric'].sel({lev_name:900,
-                                                   time_name:'{:%Y-%m-%d}'.format(dt)}).values
-            * units(ds['v-component_of_wind_isobaric'].units))
+hght_500 = ds.Geopotential_height_isobaric.metpy.sel(vertical=500 * units.hPa,
+                                                     time=vtime).metpy.unit_array
+uwnd_500 = ds['u-component_of_wind_isobaric'].metpy.sel(vertical=500 * units.hPa,
+                                                        time=vtime).metpy.unit_array
+vwnd_500 = ds['v-component_of_wind_isobaric'].metpy.sel(vertical=500 * units.hPa,
+                                                        time=vtime).metpy.unit_array
+uwnd_900 = ds['u-component_of_wind_isobaric'].metpy.sel(vertical=900 * units.hPa,
+                                                        time=vtime).metpy.unit_array
+vwnd_900 = ds['v-component_of_wind_isobaric'].metpy.sel(vertical=900 * units.hPa,
+                                                        time=vtime).metpy.unit_array
