7b0911b757cc7b0dcde5fd9f11899de9ac6be321
zbruick <zachary.bruick@valpo.edu>
2019-08-27 14:35:51 -0600

    Update QG notebook and solutions based on #451

--- notebooks/MetPy_Advanced/solutions/QG_data.py
+++ notebooks/MetPy_Advanced/solutions/QG_data.py
@@ -1,11 +1,11 @@
 # Remaining variables needed to compute QG Omega forcing terms
 hght_500 = ds.Geopotential_height_isobaric.metpy.sel(vertical=500 * units.hPa,
-                                                     time=vtime).metpy.unit_array
+                                                     time=vtime)
 uwnd_500 = ds['u-component_of_wind_isobaric'].metpy.sel(vertical=500 * units.hPa,
-                                                        time=vtime).metpy.unit_array
+                                                        time=vtime)
 vwnd_500 = ds['v-component_of_wind_isobaric'].metpy.sel(vertical=500 * units.hPa,
-                                                        time=vtime).metpy.unit_array
+                                                        time=vtime)
 uwnd_900 = ds['u-component_of_wind_isobaric'].metpy.sel(vertical=900 * units.hPa,
-                                                        time=vtime).metpy.unit_array
+                                                        time=vtime)
 vwnd_900 = ds['v-component_of_wind_isobaric'].metpy.sel(vertical=900 * units.hPa,
-                                                        time=vtime).metpy.unit_array
+                                                        time=vtime)
--- notebooks/MetPy_Advanced/solutions/term_B_calc.py
+++ notebooks/MetPy_Advanced/solutions/term_B_calc.py
@@ -1,6 +1,5 @@
 # 700-hPa Temperature Advection
-tadv_700 = mpcalc.advection(tmpk_700s, (uwnd_700s, vwnd_700s), (dx, dy),
-                            dim_order='yx').to_base_units()
+tadv_700 = mpcalc.advection(tmpk_700s, (uwnd_700s, vwnd_700s), (dx, dy)).to_base_units()
 # Laplacian of Temperature Advection
 lap_tadv_700 = mpcalc.laplacian(tadv_700, deltas=(dy, dx))
 
