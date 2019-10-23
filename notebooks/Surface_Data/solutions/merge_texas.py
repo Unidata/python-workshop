499618a6c74c5d0a0b8d5d692e733105babe289f
Eric Bruning <eric.bruning@gmail.com>
2019-10-23 10:16:15 -0500

    Update mesonet example to use datetime and related parsing

--- notebooks/Surface_Data/solutions/merge_texas.py
+++ notebooks/Surface_Data/solutions/merge_texas.py
@@ -1,4 +1,4 @@
 # Find common time between TX and OK data
 tx_data = pd.merge(tx_data, tx_stations, left_on='Station_ID', right_on='Logger ID')
-tx_one_time = tx_data[tx_data['Time'] == 2300]
+tx_one_time = tx_data[tx_data['Time'] == '2019-3-22 23:00']
 tx_one_time
